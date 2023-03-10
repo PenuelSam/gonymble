28d2981393242596b1d425735e0d83b9d118a0e1
PenuelSam <penuelsamuel54@gmail.com>
2023-03-10 18:10:54 +0100

    Fixed Styling

--- src/components/custormer/Customer.jsx
+++ src/components/custormer/Customer.jsx
@@ -2,25 +2,25 @@ import React from 'react'
 
 const Customer = () => {
   return (
-    <div className='w-[600px] md:w-[100vw] md:h-[70%] lg:h-[600px] xsm:px-[20px] h-[500px]  bg-black/70'>
+    <div className='w-[600px] md:w-[100vw] md:h-[70%] lg:h-[600px] xsm:pl-[30px] h-[500px]  bg-black/70'>
        <h1 className='text-[30px] text-green-500 font-normal pt-[30px] '>Contact Us</h1>
        <div className='w-full h-[1px] bg-white'></div>
        <form className='flex flex-col justify-center items-center  h-full '>
         <div className='flex items-center md:flex-col md:items-start justify-between gap-[20px] py-[10px] mr-[20%] '>
           <label className='font-normal text-slate-200 lg:text-[25px] md:text-[20px]'>Name:</label>
-          <input type="text" placeholder='Name' className='ml-[60px] md:ml-0 text-white bg-transparent border py-[5px] px-[10px] outline-none' />
+          <input type="text" placeholder='Name' className='bg-transparent  text-white border py-[5px] px-[5px] outline-none' />
         </div>
         <div className='flex items-center md:flex-col md:items-start justify-between gap-[20px] py-[10px]  mr-[20%] '>
           <label className='font-normal text-slate-200 lg:text-[25px] md:text-[20px]'>Email:</label>
-          <input type="email" placeholder='Email' className='ml-[70px] md:ml-0  text-white bg-transparent border py-[5px] px-[10px] outline-none' />
+          <input type="email" placeholder='Email' className='bg-transparent  text-white border py-[5px] px-[5px] outline-none' />
         </div>
         <div className='flex items-center md:flex-col md:items-start justify-between gap-[20px] py-[10px]  mr-[20%]'>
           <label className='font-normal text-slate-200 lg:text-[25px] md:text-[20px]'>Phone Number:</label>
-          <input type="number" placeholder='PhoneNumber' className='bg-transparent  text-white border py-[5px] px-[10px] outline-none' />
+          <input type="number" placeholder='PhoneNumber' className='bg-transparent  text-white border py-[5px] px-[5px] outline-none' />
         </div>
         <div className='flex items-center md:flex-col md:items-start justify-between gap-[20px] py-[10px]  mr-[20%]'>
           <label className='font-normal text-slate-200 lg:text-[25px] md:text-[20px]'>Message:</label>
-          <textarea cols="25" placeholder='Message' className='overflow-hidden md:ml-0  text-white ml-[60px] bg-transparent border py-[5px] px-[10px] outline-none'></textarea>
+          <textarea cols="25" placeholder='Message' className='bg-transparent  text-white border py-[5px] px-[5px] outline-none overflow-hidden'></textarea>
         </div>
         <button className='btn-theme bgc m-[20px] md:ml-0 ml-[20%] text-white lg:text-[25px] md:text-[20px]'>Message</button>
        </form>
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -5,10 +5,10 @@ const Header = () => {
     <div className='header'>
       <div className="container flex relative top-[30%] lg:top-[40%]  justify-between items-center">
             <div>
-            <h1 className='text-white w-fit leading-[1.7] font-bold text-[3rem] lg:text-[4rem] md:text-[1.5rem] pr-[20px] xsm:text-[1.3rem] ml-0 lg:ml-[15px]'>Gonymble – simple, efficient, and dependable delivery for your customers.</h1>
-            <div className='mt-[15px] flex items-center ml-0 lg:ml-[15px]'>
-              <input type="text" placeholder='Track your package' className='p-[5px] px-[10px] outline-none border-none lg:p-[10px] lg:px-[15px] md:p-[5px] md:px-[10px] xsm:px-[5px]  text-[18px] lg:text-[30px] md:text-[20px]  input' />
-              <button className='text-white p-[5px] px-[10px] lg:p-[10px] md:p-[5px] xsm:px-[8px] md:px-[15px] text-[18px] lg:text-[30px] md:text-[20px] btn'>Track</button>
+            <h1 className='text-white w-fit leading-[1.7] font-bold text-[3rem] lg:text-[4rem] md:text-[2rem] pr-[20px] xsm:text-[1.4rem] ml-0 lg:ml-[15px] md:ml-[8px]'>Gonymble – simple, efficient, and dependable delivery for your customers.</h1>
+            <div className='mt-[15px] flex items-center ml-0 lg:ml-[15px] md:ml-[8px]'>
+              <input type="text" placeholder='Track your package' className='p-[5px] px-[10px] outline-none border-none lg:p-[10px] lg:px-[15px] md:p-[5px] md:px-[5px] xsm:px-[5px]  text-[18px] lg:text-[30px] md:text-[15px]  input' />
+              <button className='text-white p-[5px] px-[10px] lg:p-[10px] md:p-[5px] xsm:px-[8px] md:px-[15px] text-[18px] lg:text-[30px] md:text-[15px] btn'>Track</button>
             </div>
             </div>
 
